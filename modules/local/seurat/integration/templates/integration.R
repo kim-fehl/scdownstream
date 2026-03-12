f143d13573f2cd5c499e8c69ded148291c6b19fd
kim-fehl <kim@kim.fehl>
2026-03-12 16:39:46 +0400

    fix(SEURAT_INTEGRATION): use task cpus for integration parallelism

--- modules/local/seurat/integration/templates/integration.R
+++ modules/local/seurat/integration/templates/integration.R
@@ -6,6 +6,23 @@ library(anndataR)
 # Set random seed
 set.seed(0)
 
+num_threads <- max(1L, as.integer("${task.cpus}"))
+Sys.setenv(
+  OMP_NUM_THREADS = num_threads,
+  OPENBLAS_NUM_THREADS = num_threads,
+  MKL_NUM_THREADS = num_threads,
+  BLIS_NUM_THREADS = num_threads,
+  VECLIB_MAXIMUM_THREADS = num_threads,
+  RCPP_PARALLEL_NUM_THREADS = num_threads
+)
+
+if (num_threads > 1L && requireNamespace("future", quietly = TRUE)) {
+  future_strategy <- ifelse(.Platform$OS.type == "unix",
+                            "multicore", "multisession")
+  future::plan(future_strategy, workers = num_threads)
+  on.exit(future::plan("sequential"), add = TRUE)
+}
+
 adata <- read_h5ad("${h5ad}")
 
 seurat_obj <- adata\$as_Seurat()
